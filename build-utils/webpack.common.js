a86ecfa4adc7590a9d2c4fd6c941c120f6bef345
maudnals <maud.nalpas@gmail.com>
2018-05-06 14:40:35 +0200

    added webpack html plugin

--- build-utils/webpack.common.js
+++ build-utils/webpack.common.js
@@ -1,12 +1,18 @@
 const path = require('path');
 const commonPaths = require('./common-paths');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const config = {
   entry: './src/',
   output: {
     filename: 'bundle.js',
     path: commonPaths.outputPath
-  }
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin(),
+  ]
 };
 
 module.exports = config;
