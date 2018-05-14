ba6b242d1196211a2feb5440d227f5efe374ca6a
maudnals <maud.nalpas@gmail.com>
2018-05-14 18:40:27 +0200

    added webpack dev server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import createCard from './card';
 
 window.alert(createCard(1, 3));
+let div = document.createElement("div");
+div.innerHTML = "hi";
+document.querySelector("body").appendChild(div);
